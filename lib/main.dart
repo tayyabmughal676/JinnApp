2509b698fb9359bcedc01a2aad056490b02d658a
tayyabmughal676 <tayyabmughal676@gmail.com>
2021-07-27 18:40:55 +0500

    vfghf

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
     const title = 'Get My Position';
     return MaterialApp(
       title: title,
+
       home: Scaffold(
           appBar: AppBar(
             title: const Text(title),
