aebaf4cd54ce6eda58b245771a56ea6330b92914
tayyabmughal676 <tayyabmughal676@gmail.com>
2021-07-27 18:36:06 +0500

    hello

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:get_position/screens/HomePage.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    const title = 'Get My Position';
+    return MaterialApp(
+      title: title,
+      home: Scaffold(
+          appBar: AppBar(
+            title: const Text(title),
+            backgroundColor: Colors.deepOrange,
+          ),
+          body: HomePage()),
+    );
+  }
+}
